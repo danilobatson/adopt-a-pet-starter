4c9a183f36ec874b0fe17bb850c5be4bb02dfb4e
danilobatson <djbatson19@gmail.com>
2021-09-22 20:33:29 -0700

    starter complete

--- src/App.js
+++ src/App.js
@@ -15,9 +15,20 @@ function App() {
   return (
     <Router>
       <Navigation />
-      <Route path="/:type?">
-      <HomePage />
-      </Route>
+      <Switch>
+        <Route path="pet-details-not-found">
+          <PetDetailsNotFound />
+        </Route>
+        <Route path="/:type/:id">
+          <PetDetailsPage />
+        </Route>
+        <Route path="/:type?">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
